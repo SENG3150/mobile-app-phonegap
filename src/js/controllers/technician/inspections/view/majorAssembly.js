02be532cd81ac0c62fd205821cfb0dbd1ea35ae2
Moten <motenkingsmill@hotmail.com>
2016-08-12 08:55:06 +1000

    Working back button
    Working back button for major assembly

--- src/js/controllers/technician/inspections/view/majorAssembly.js
+++ src/js/controllers/technician/inspections/view/majorAssembly.js
@@ -1,12 +1,13 @@
 angular
 	.module('joy-global')
-	.controller('TechnicianInspectionsViewControllerMajorAssembly', ['$scope', '$stateParams', function ($scope, $stateParams) {
+	.controller('TechnicianInspectionsViewControllerMajorAssembly', ['$scope', '$stateParams', 'moment', 'Inspections', 'LayoutService', function ($scope, $stateParams, moment, Inspections, LayoutService) {
 		$scope.loading = true;
 
-        LayoutService.setTitle(['Major Assemblies']);
-        LayoutService.getPageHeader().setBackButton(LayoutService.redirect('technician-inspections-index'));
+        $scope.inspectionId = $stateParams.inspection;
 
-		$scope.inspectionId = $stateParams.inspection;
+        LayoutService.setTitle(['Major Assemblies']);
+        LayoutService.getPageHeader().setBackButton(LayoutService.redirect('technician-inspections-view-index', { inspection: $scope.inspectionId }));
+
 		$scope.majorAssemblyId = $stateParams.majorAssembly;
 
 		$scope.loading = false
