0364b3dc4c3a0fdb61cb6ff04f5be54b7908d35f
Moten <motenkingsmill@hotmail.com>
2016-08-12 05:58:21 +1000

    Inspection view fixes
    Fixed the pre tag and the accidental script tag.

--- src/js/controllers/technician/inspections/view/majorAssembly.js
+++ src/js/controllers/technician/inspections/view/majorAssembly.js
@@ -2,6 +2,10 @@ angular
 	.module('joy-global')
 	.controller('TechnicianInspectionsViewControllerMajorAssembly', ['$scope', '$stateParams', function ($scope, $stateParams) {
 		$scope.loading = true;
+
+        LayoutService.setTitle(['Major Assemblies']);
+        LayoutService.getPageHeader().setBackButton(LayoutService.redirect('technician-inspections-index'));
+
 		$scope.inspectionId = $stateParams.inspection;
 		$scope.majorAssemblyId = $stateParams.majorAssembly;
 
