51a9daa8ef35be2fc84ca8d63206d02eec320dd6
DESKTOP-QTES9E0\bbaxt <bbaxter11494@gmail.com>
2016-08-12 22:46:26 +1000

    Added dynamic back button to Create Inspection

--- src/js/controllers/technician/inspections/create/index.js
+++ src/js/controllers/technician/inspections/create/index.js
@@ -2,7 +2,9 @@ angular
 	.module('joy-global')
 	.controller('TechnicianInspectionsCreateControllerIndex', ['$scope', 'LayoutService', function ($scope, LayoutService) {
 		LayoutService.setTitle(['Create Inspection', 'Inspections']);
-		LayoutService.getPageHeader().setBackButton(LayoutService.redirect('technician-inspections-index'));
+		LayoutService.getPageHeader().setBackButton(function () {
+			history.back();
+		});
 		LayoutService.getPageHeader().setHeroButton('fa fa-fw fa-check', 'Save', function() {
 			alert('Saved!');
 		});
