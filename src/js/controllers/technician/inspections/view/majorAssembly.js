5b25815289ef7814993fd839e6a9f045677422b3
Moten <motenkingsmill@hotmail.com>
2016-08-13 14:03:06 +1000

    Major Assemblies Progress
    Still not working

--- src/js/controllers/technician/inspections/view/majorAssembly.js
+++ src/js/controllers/technician/inspections/view/majorAssembly.js
@@ -2,13 +2,32 @@ angular
 	.module('joy-global')
 	.controller('TechnicianInspectionsViewControllerMajorAssembly', ['$scope', '$stateParams', 'moment', 'Inspections', 'LayoutService', function ($scope, $stateParams, moment, Inspections, LayoutService) {
 		$scope.loading = true;
-
         $scope.inspectionId = $stateParams.inspection;
+        $scope.majorAssemblyId = $stateParams.majorAssembly;
+        $scope.moment = moment;
 
         LayoutService.setTitle(['Major Assemblies']);
         LayoutService.getPageHeader().setBackButton(LayoutService.redirect('technician-inspections-view-index', { inspection: $scope.inspectionId }));
 
-		$scope.majorAssemblyId = $stateParams.majorAssembly;
+        //Select the specific inspection
+        Inspections
+            .one($scope.inspectionId)
+            .get({
+                include: 'technician,scheduler,machine.model,majorAssemblies.majorAssembly,majorAssemblies.subAssemblies.subAssembly'
+            })
+            .then(function (data) {
+                $scope.inspection = data;
+            });
 
+        //Select the specific major Assembly
+        angular.forEach($scope.inspection.majorAssemblies, function(majorAssembly) {
+            console.log(majorAssembly.id);
+            if(majorAssembly.id == $scope.majorAssemblyId) {
+                // You've got a match
+                console.log(majorAssembly);
+                console.log("DOG");
+                $scope.majorAssembly = majorAssembly;
+            }
+        });
 		$scope.loading = false
 	}]);
