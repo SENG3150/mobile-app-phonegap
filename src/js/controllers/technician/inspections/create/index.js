0b48583ad9aa6f69771afcb48845e60f8c6d0cd2
DESKTOP-QTES9E0\bbaxt <bbaxter11494@gmail.com>
2016-08-14 00:31:04 +1000

    Created functionality to display machine information from Create Inspection drop-down menu

--- src/js/controllers/technician/inspections/create/index.js
+++ src/js/controllers/technician/inspections/create/index.js
@@ -3,16 +3,14 @@ angular
 	.controller('TechnicianInspectionsCreateControllerIndex', ['$scope', 'Machines', 'moment', 'LayoutService', 'NotificationService', function ($scope, Machines, moment, LayoutService, NotificationService) {
 
 		LayoutService.setTitle(['Create Inspection', 'Machines']);
-		LayoutService.getPageHeader().setBackButton(function () {
-			history.back();
-		});
+		LayoutService.getPageHeader().setBackButton(LayoutService.redirect('technician-inspections-index'));
 		LayoutService.getPageHeader().setHeroButton('fa fa-fw fa-check', 'Save', function() {
 			NotificationService.alert('Saved!');
 		});
 
 		Machines
 			.getList({
-				include: 'model'
+				include: 'model,model.majorAssemblies,model.majorAssemblies.subAssemblies'
 			})
 			.then(function (data) {
 				$scope.loading = false;
