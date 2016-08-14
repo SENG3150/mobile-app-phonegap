5e60c7b69cf164c05a9248cc08ac49df044182d0
Mitchell Davis <mitchell.peter.davis@gmail.com>
2016-08-15 00:45:10 +1000

    Added logout button to settings page.

--- src/js/controllers/technician/settings/index.js
+++ src/js/controllers/technician/settings/index.js
@@ -2,4 +2,5 @@ angular
 	.module('joy-global')
 	.controller('TechnicianSettingsControllerIndex', ['$scope', 'LayoutService', function ($scope, LayoutService) {
 		LayoutService.setTitle('Settings');
+		LayoutService.getPageHeader().setLeftButton('fa fa-fw fa-sign-out', 'Logout', LayoutService.redirect('auth.logout'));
 	}]);
