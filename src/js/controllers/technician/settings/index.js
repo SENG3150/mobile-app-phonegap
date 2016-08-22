7d9ffdf34a8ac6f6c55089facb8688fa56a1e654
Nicolas Blarasin <nicolas.blarasin@gmail.com>
2016-08-23 09:15:57 +1000

    Added settings service

--- src/js/controllers/technician/settings/index.js
+++ src/js/controllers/technician/settings/index.js
@@ -1,6 +1,8 @@
 angular
 	.module('joy-global')
-	.controller('TechnicianSettingsControllerIndex', ['$scope', 'LayoutService', function ($scope, LayoutService) {
+	.controller('TechnicianSettingsControllerIndex', ['$scope', 'LayoutService', function ($scope, LayoutService, SettingsService) {
 		LayoutService.setTitle('Settings');
 		LayoutService.getPageHeader().setHeroButton('fa fa-fw fa-sign-out', 'Logout', LayoutService.redirect('auth.logout'));
+
+		SettingsService.set("autoSync", true);
 	}]);
--- src/js/services/SettingsService.js
+++ src/js/services/SettingsService.js
@@ -1,12 +1,10 @@
 angular
     .module('joy-global')
     .service('SettingsService', ['$localStorage', function($localStorage) {
-        // key : String, value : String
         this.set = function(key, value) {
             $localStorage.settings[key] = value;
         };
 
-        // key : String, return : String
         this.get = function(key) {
             return $localStorage.settings[key];
         };
