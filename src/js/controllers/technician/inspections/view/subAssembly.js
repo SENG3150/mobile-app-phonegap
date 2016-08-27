b9b4f3f1ea286c1cf9db33c04c6c75f10dc6d507
Moten <motenkingsmill@hotmail.com>
2016-08-27 18:00:59 +1000

    Saving
    Saving is now working. Need to test photos on device.

--- src/js/controllers/technician/inspections/view/subAssembly.js
+++ src/js/controllers/technician/inspections/view/subAssembly.js
@@ -6,39 +6,27 @@ angular
 		$scope.majorAssemblyId = $stateParams.majorAssembly;
 		$scope.subAssemblyId = $stateParams.subAssembly;
 		$scope.moment = moment;
-		$scope.showComments = false;
+		$scope.showOilComments = false;
+        $scope.showMachineComments = false;
+        $scope.showWearComments = false;
         $scope.inspection = InspectionsStorage.one($scope.inspectionId);
-        $scope.oilPhotos = [];
+        $scope.status = {wear: 'OK', oil: 'OK', machine: 'OK'};
 
 
-		//Method managing the changing of the action status for the oilTest
-		$scope.changeOilStatus = function (input) {
-		    if(input==null){
-                switch ($scope.oilStatus) {
-                    case 1:
-                        return "OK";
-                    case 2:
-                        return "WARNING";
-                    case 3:
-                        return "ACTION";
-                }
+		//Method managing the changing of the action status for tests
+		$scope.changeActionStatus = function (input, type) {
+            switch(type){
+                case 1:
+                    $scope.status.oil = input;
+                    break;
+                case 2:
+                    $scope.status.machine = input;
+                    break;
+                case 3:
+                    $scope.status.wear = input;
+                    break;
             }
-			$scope.oilStatus = input;
 		};
-		//Method managing the changing of the action status for the machineGeneralTest
-        $scope.changemachineGeneralStatus = function (input) {
-            if(input==null){
-                switch ($scope.machineStatus) {
-                    case 1:
-                        return "OK";
-                    case 2:
-                        return "WARNING";
-                    case 3:
-                        return "ACTION";
-                }
-            }
-            $scope.machineStatus = input;
-        };
 
 		//Set the back button
 		LayoutService.getPageHeader().setBackButton(LayoutService.redirect('technician-inspections-view-majorAssembly', {
@@ -78,7 +66,7 @@ angular
         if(typeof $scope.oilTest != 'undefined') { //Load the existing oilTest data if it exists, otherwise create empty data
             $scope.oilComments = $scope.oilTest.comments; //Load the comments
             $scope.oilPhotos = $scope.oilTest.photos; //Load the photos
-            if ($scope.oilTest.actionItem == null) { //If there is no action item, create an empty one so that we can populate the text boxes with nothing (this won't be saved)
+            if ($scope.oilTest.actionItem == null) { //If there is no action item, create an empty one so that we can populate the text boxes with nothing
                 $scope.oilTest.actionItem = {issue: '', status: '', action: ''};
             }
             $scope.oil = { //Fill the text boxes with the previous info
@@ -97,25 +85,12 @@ angular
                 action: $scope.oilTest.actionItem.action,
                 comment: $scope.oilComments[$scope.oilComments.length - 1].text
             }
-            switch ($scope.oilTest.actionItem.status) { //Method for reloading the state of the oil action item. Needs to be a switch to process the text status
-                case 'OK':
-                    $scope.changeOilStatus(1);
-                    break;
-                case 'WARNING':
-                    $scope.changeOilStatus(2);
-                    break;
-                case 'ACTION':
-                    $scope.changeOilStatus(3);
-                    break;
-                default:
-                    $scope.changeOilStatus(1);
-                    break;
-            }
+            $scope.changeActionStatus($scope.oilTest.actionItem.status, 1);
         }
         else {
             $scope.oilComments = []; //Create an empty array of comments
             $scope.oilPhotos = []; //Create an empty array of photos
-            $scope.changeOilStatus(1); //Default the status to OK
+            $scope.changeActionStatus('OK', 1); //Default the status to OK
             $scope.oil = { //Fill the text boxes with blank info
                 id: 1,
                 lead: null,
@@ -134,46 +109,73 @@ angular
             }
         }
 
-        if(typeof $scope.machineGeneralTest != 'undefined' && $scope.machineGeneralTest.actionItem != null) { //Load the existing machineGeneralTest data if it exists, otherwise create empty data
+        if(typeof $scope.machineGeneralTest != 'undefined') { //Load the existing machineGeneralTest data if it exists, otherwise create empty data
+            if ($scope.machineGeneralTest.actionItem == null) { //If there is no action item, create an empty one so that we can populate the text boxes with nothing
+                $scope.machineGeneralTest.actionItem = {issue: '', status: '', action: ''};
+            }
+            $scope.machinePhotos = $scope.machineGeneralTest.photos;
+            $scope.machineComments = $scope.machineGeneralTest.comments;
+            $scope.changeActionStatus($scope.machineGeneralTest.actionItem.status, 2)
             $scope.machineTest = { //Fill the text boxes with the previous info
                 issue: $scope.machineGeneralTest.actionItem.issue,
-                action: $scope.machineGeneralTest.actionItem.action
-            }
-            switch ($scope.machineGeneralTest.actionItem.status) { //Method for reloading the state of the machine general action item. Needs to be a switch to process the text status
-                case 'OK':
-                    $scope.changemachineGeneralStatus(1);
-                    break;
-                case 'WARNING':
-                    $scope.changemachineGeneralStatus(2);
-                    break;
-                case 'ACTION':
-                    $scope.changemachineGeneralStatus(3);
-                    break;
-                default:
-                    $scope.changemachineGeneralStatus(1);
-                    break;
+                action: $scope.machineGeneralTest.actionItem.action,
+                comment: $scope.machineComments[$scope.machineComments.length - 1].text
             }
         }
         else {
-            $scope.changemachineGeneralStatus(1); //Default the status to OK
+            $scope.machinePhotos = [];
+            $scope.machineComments = [];
+            $scope.changeActionStatus('OK', 2); //Default the status to OK
             $scope.machineTest = { //Fill the text boxes with the previous info
                 issue: '',
-                action: ''
+                action: '',
+                comment: ''
+            }
+        }
+
+        if(typeof $scope.wearTest != 'undefined') { //Load the existing wearTest data if it exists, otherwise create empty data
+            if ($scope.wearTest.actionItem == null) { //If there is no action item, create an empty one so that we can populate the text boxes with nothing
+                $scope.wearTest.actionItem = {issue: '', status: '', action: ''};
+            }
+            $scope.wearPhotos = $scope.wearTest.photos;
+            $scope.wearComments = $scope.wearTest.comments;
+            $scope.changeActionStatus($scope.wearTest.actionItem.status, 3);
+            $scope.wearDetails = [];
+            angular.forEach($scope.wearTest.uniqueDetails, function (uniqueDetail) {
+                $scope.wearDetails.push(uniqueDetail);
+            });
+            $scope.wear = { //Fill the text boxes with the previous info
+                SMU: $scope.wearTest.smu,
+                detail: $scope.wearDetails,
+                issue: $scope.wearTest.actionItem.issue,
+                action: $scope.wearTest.actionItem.action,
+                comment: $scope.wearComments[$scope.wearComments.length - 1].text
+            }
+        }
+        else {
+            $scope.wearComments = [];
+            $scope.wearPhotos = [];
+            $scope.changeActionStatus('OK', 3); //Default the status to OK
+            $scope.wear = { //Fill the text boxes with the previous info
+                SMU: null,
+                detail: [],
+                issue: '',
+                action: '',
+                comment: ''
             }
         }
 
 
         //Method managing the photo taking (Quality is set to 50 at the moment, this may change based on file size requirements and quality needs)
-        //Photos need testing on iOS device!!!
         $scope.takePhoto = function (input) {
-            navigator.camera.getPicture( onSuccess, onFail, {
-                quality: 50, 
+            navigator.camera.getPicture( onSuccess(input), onFail, {
+                quality: 50,
                 destinationType: Camera.DestinationType.DATA_URL
             });
         };
         //Take photo succeeded
-        function onSuccess(imageData) {
-            $scope.oilPhotos.push({
+        function onSuccess(imageData, location) {
+            location.push({
                 format: 'jpeg',
                 photo: imageData
             });
@@ -201,16 +203,17 @@ angular
         //Function for saving changes to the sub Assembly
 		$scope.saveSubAssembly = function(oil, machineTest, wear) {
             //Generate OilTest Item
+
             if($scope.subAssembly.subAssembly.oil != false) {
                 $scope.oilActionItem = { //Generate action item
                     id: 1,
-                    status: $scope.changeOilStatus(null),
+                    status: $scope.status.oil,
                     issue: oil.issue,
                     action: oil.action,
                     timeActioned: moment(),
                     technician: $scope.inspection.technician
                 }
-                if ($scope.changeOilStatus(null) == 'OK') { //Remove action item if the state is set to 'OK' (Because then it isn't an issue)
+                if ($scope.status.oil == 'OK') { //Remove action item if the state is set to 'OK' (Because then it isn't an issue)
                     $scope.oilActionItem.issue = '';
                     $scope.oilActionItem.action = '';
                 }
@@ -221,7 +224,6 @@ angular
                         text: oil.comment,
                         author: $scope.inspection.technician
                     }
-                    console.log($scope.oilComments.length);
                     if ($scope.oilComments.length == 0) {
                         $scope.oilComments.push($scope.oilCommentNew);
                     }
@@ -249,25 +251,86 @@ angular
                     actionItem: $scope.oilActionItem
                 }
             }
-            //Generate Wear Test Item
-
             //Generate Machine General Test Item
             if($scope.subAssembly.subAssembly.machineGeneral != false) {
                 $scope.machineActionItem = { //Generate action item
                     id: 1,
-                    status: $scope.changemachineGeneralStatus(null),
+                    status: $scope.status.machine,
                     issue: machineTest.issue,
                     action: machineTest.action,
                     timeActioned: moment(),
                     technician: $scope.inspection.technician
                 }
-                if ($scope.changemachineGeneralStatus(null) == 'OK') { //Remove action item issue and action if the state is set to 'OK' (Because then it isn't an issue)
+                if ($scope.status.machine == 'OK') { //Remove action item issue and action if the state is set to 'OK' (Because then it isn't an issue)
                     $scope.machineActionItem.issue = '';
                     $scope.machineActionItem.action = '';
                 }
+                if (machineTest.comment != null) {
+                    $scope.machineCommentNew = {
+                        id: $scope.machineComments.length + 1,
+                        timeCommented: moment(),
+                        text: machineTest.comment,
+                        author: $scope.inspection.technician
+                    }
+                    if ($scope.machineComments.length == 0) {
+                        $scope.machineComments.push($scope.machineCommentNew);
+                    }
+                    else {
+                        if (machineTest.comment != $scope.machineComments[$scope.machineComments.length - 1].text) {
+                            $scope.machineComments.push($scope.machineCommentNew);
+                        }
+                    }
+                }
                 $scope.machineGeneralTestSave = {
                     id: 1,
+                    comments: $scope.machineComments,
+                    photos:  $scope.machinePhotos,
                     actionItem: $scope.machineActionItem
+                }
+            }
+            //Generate Wear Test Item
+            if($scope.subAssembly.subAssembly.wear != false) {
+                $scope.wearActionItem = { //Generate action item
+                    id: 1,
+                    status: $scope.status.wear,
+                    issue: wear.issue,
+                    action: wear.action,
+                    timeActioned: moment(),
+                    technician: $scope.inspection.technician
+                }
+                if ($scope.status.wear == 'OK') { //Remove action item issue and action if the state is set to 'OK' (Because then it isn't an issue)
+                    $scope.wearActionItem.issue = '';
+                    $scope.wearActionItem.action = '';
+                }
+                if (wear.comment != null) {
+                    $scope.wearCommentNew = {
+                        id: $scope.wearComments.length + 1,
+                        timeCommented: moment(),
+                        text: wear.comment,
+                        author: $scope.inspection.technician
+                    }
+                    console.log($scope.wearComments.length);
+                    if ($scope.wearComments.length == 0) {
+                        $scope.wearComments.push($scope.wearCommentNew);
+                    }
+                    else {
+                        if (wear.comment != $scope.wearComments[$scope.wearComments.length - 1].text) {
+                            $scope.wearComments.push($scope.wearCommentNew);
+                        }
+                    }
+                }
+                $scope.wearUniqueDetails = {};
+                for (var i = 0; i < wear.detail.length; i += 1){
+                    var temp = $scope.subAssembly.subAssembly.uniqueDetails[i];
+                    $scope.wearUniqueDetails[temp] = wear.detail[i];
+                }
+                $scope.wearTestSave = {
+                    id: 1,
+                    smu: wear.SMU,
+                    uniqueDetails: $scope.wearUniqueDetails,
+                    comments: $scope.wearComments,
+                    photos:  $scope.wearPhotos,
+                    actionItem: $scope.wearActionItem
                 }
             }
             //Loop through to find the subAssembly to overwrite
@@ -282,6 +345,9 @@ angular
                             }
                             if($scope.subAssembly.subAssembly.machineGeneral != false) { //Save the completed oilTest
                                 subAssembly.machineGeneralTest = $scope.machineGeneralTestSave;
+                            }
+                            if($scope.subAssembly.subAssembly.wearTest != false) { //Save the completed oilTest
+                                subAssembly.wearTest = $scope.wearTestSave;
                             }
                         }
                     });
