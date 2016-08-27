a07c89a938c0fb9a450c8291c0f19f12d496f57b
Moten <motenkingsmill@hotmail.com>
2016-08-27 14:55:58 +1000

    Photo saving
    People shouldn't commit this much.

--- src/js/controllers/technician/inspections/view/subAssembly.js
+++ src/js/controllers/technician/inspections/view/subAssembly.js
@@ -166,7 +166,10 @@ angular
         //Method managing the photo taking (Quality is set to 50 at the moment, this may change based on file size requirements and quality needs)
         //Photos need testing on iOS device!!!
         $scope.takePhoto = function (input) {
-            navigator.camera.getPicture( onSuccess, onFail, {quality: 50, DATA_URL: 0});
+            navigator.camera.getPicture( onSuccess, onFail, {
+                quality: 50, 
+                destinationType: Camera.DestinationType.DATA_URL
+            });
         };
         //Take photo succeeded
         function onSuccess(imageData) {
