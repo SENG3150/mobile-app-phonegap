8cc3cabf3d5468323557a0c014a60e6b27834b6a
Mitchell Davis <mitchell.peter.davis@gmail.com>
2016-08-29 00:19:31 +1000

    Removed misleading hero button.

--- src/js/controllers/technician/inspections/view/index.js
+++ src/js/controllers/technician/inspections/view/index.js
@@ -1,14 +1,11 @@
 angular
 	.module('joy-global')
-	.controller('TechnicianInspectionsViewControllerIndex', ['$scope', 'InspectionsStorage', 'moment', '$stateParams', 'LayoutService', 'NotificationService', function ($scope, InspectionsStorage, moment, $stateParams, LayoutService, NotificationService) {
+	.controller('TechnicianInspectionsViewControllerIndex', ['$scope', 'InspectionsStorage', 'moment', '$stateParams', 'LayoutService', function ($scope, InspectionsStorage, moment, $stateParams, LayoutService) {
 		$scope.inspectionId = $stateParams.inspection;
 		$scope.showMajorAssemblies = true;
 
 		LayoutService.setTitle(['Inspection ' + $scope.inspectionId, 'Inspections']);
 		LayoutService.getPageHeader().setBackButton(LayoutService.redirect('technician-inspections-index'));
-		LayoutService.getPageHeader().setHeroButton('fa fa-fw fa-check', 'Save', function () {
-			NotificationService.alert('Saved!');
-		});
 
 		$scope.moment = moment;
 		$scope.inspection = InspectionsStorage.one($scope.inspectionId);
