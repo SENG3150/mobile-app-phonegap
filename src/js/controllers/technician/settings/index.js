518d82f295e3f6943867e6679077d964daed32f9
Moten <motenkingsmill@hotmail.com>
2016-08-30 23:26:53 +1000

    Re added reset button.
    I'm an idiot.

--- src/js/controllers/technician/settings/index.js
+++ src/js/controllers/technician/settings/index.js
@@ -1,8 +1,16 @@
 angular
 	.module('joy-global')
-	.controller('TechnicianSettingsControllerIndex', ['$scope', 'LayoutService', 'SettingsService', 'ToggleService', function ($scope, LayoutService, SettingsService, ToggleService) {
+	.controller('TechnicianSettingsControllerIndex', ['$scope', 'LayoutService', 'SettingsService', 'ToggleService', 'InspectionsStorage', 'NotificationService', function ($scope, LayoutService, SettingsService, ToggleService, InspectionsStorage, NotificationService) {
 		LayoutService.setTitle('Settings');
 		LayoutService.getPageHeader().setHeroButton('fa fa-fw fa-sign-out', 'Logout', LayoutService.redirect('auth.logout'));
+        LayoutService.getPageHeader().setLeftButton('fa fa-fw fa-download', 'Reset', function() {
+            NotificationService.confirm('This will delete all inspection data from this device. Please ensure that ' +
+                'you sync any important data before confirming.', $scope.inspectionReset());
+        });
+
+        $scope.inspectionReset = function() {
+            InspectionsStorage.reset(true);
+        };
 
 		$scope.settingsService = SettingsService;
 
