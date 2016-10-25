8b36e15d5d117f2795ebdcf03e1fad0cecf7c325
Nicolas Blarasin <nicolas.blarasin@gmail.com>
2016-10-25 14:29:22 +1100

    ''

--- src/tests/presentation/technician/inspections/index.js
+++ src/tests/presentation/technician/inspections/index.js
@@ -1,3 +1,73 @@
 // TODO: Test whether clicking on the inspection will take the user to the view page for that inspection
 
 // TODO: Test whether clicking on the New Inspection button will take the user to the create inspection page
+
+describe("inspection clicking", function() {
+    beforeEach(function() {
+        browser.get('/#/technician/inspections');
+    });
+
+    describe('click inspection redirect', function() {
+        var $httpBackend;
+        var AuthService;
+
+        var inspectionRequest = 'inspections?include=technician,scheduler,machine.model,majorAssemblies.majorAssembly,majorAssemblies.subAssemblies.subAssembly';
+        var inspectionResponse = {
+            id: 0,
+            comments: [],
+            photos: [],
+            majorAssemblies: [
+                {
+                    id: 1,
+                    photos: [],
+                    majorAssembly: {
+                        name: 'majorAssembly1'
+                    },
+                    comments: [],
+                    subAssemblies: [
+                        {
+                            id: 1,
+                            photos: [],
+                            majorAssembly: {
+                                name: 'subAssembly1'
+                            },
+                            comments: [],
+                            oilTest: {},
+                            wearTest: {} 
+                        }
+                    ]
+                }
+            ]
+        };
+
+        // tests require inspections in list for test to be valid
+        beforeEach(inject(function(_$httpBackend_, _AuthService_) {
+            $httpBackend = _$httpBackend_;
+            AuthService = _AuthService_
+
+            spyOn(AuthService, 'getUser').and.returnValue({
+                primary: {
+                    id: 10
+                }
+            });
+
+            // mock response
+            $httpBackend.when('GET', ENV.apiEndpoint + inspectionRequest).respond(inspectionData);
+        }));
+
+        it('should redirect to inspection view', function() {
+            // click first list item
+            var link = element(by.class('navigate-right'));
+            var uri = element.attributes['href'].value;
+            link.click();
+            expect(browser.getLocationAbsUrl()).toMatch(uri);
+        });
+    });
+
+    describe('click new inspection redirect', function() {
+        it('should redirect to inspection view', function() {
+            element(by.class('btn-nav')).click();
+            expect(browser.getLocationAbsUrl()).toMatch('/#/technician/inspections/create');
+        });
+    });
+});
--- src/tests/presentation/technician/inspections/view/subAssembly.js
+++ src/tests/presentation/technician/inspections/view/subAssembly.js
@@ -1,1 +1,10 @@
 // TODO: Test whether the comments and photos tabs are present on the page
+describe('test tabs present', function() {
+    it('photos tab should exist', function() {
+        
+    });
+
+    it('comments tab should exist', function() {
+
+    });
+});
